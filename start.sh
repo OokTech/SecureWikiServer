2524f928e5ff7de01d8d42c334d17c798249a67b
inmysocks <inmysocks@fastmail.com>
2018-06-10 11:11:53 -0500

    Add the Bob plugin as a submodule

--- start.sh
+++ start.sh
@@ -3,4 +3,6 @@
 # This starts the node server using nohup so it stays up until the server is
 # restarted or it is manually stopped using stop.sh
 
+export TIDDLYWIKI_PLUGIN_PATH="./Plugins"
+
 nohup node ./index.js &
