45c33f5e528e23e3b8aa76ba71fcd220195a9574
TiddlyUser <tiddlyuser@ps598467.dreamhostps.com>
2018-09-13 13:59:05 -0700

    Update start.sh to use the new startup mechanism and add output.txt to .gitignore

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@
 # restarted or it is manually stopped using stop.sh
 # The process id is stored in wikiserver.pid so that the process can be stopped
 # without trouble is stop.sh
+source ../.bashrc
 
 nohup node ./index.js &
 
